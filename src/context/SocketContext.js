362069e6c7b31452658785fb15eb4a1457bbb36b
Jeffry Delgado Sanchez <jefdel21@hotmail.com>
2023-01-24 13:06:48 -0600

    Update

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -8,8 +8,8 @@ export const SocketContext = createContext();
 export const SocketProvider = ({ children }) => {
 
     // const { socket, online } = useSocket('http://localhost:8080');
-    const { socket, online } = useSocket('https://bandnames-frontend.netlify.app/');
-    // const { socket, online } = useSocket('https://bandnames-server.netlify.app/');
+    // const { socket, online } = useSocket('https://bandnames-frontend.netlify.app/');
+    const { socket, online } = useSocket('https://bandnames-server.netlify.app/');
 
     return (
         <SocketContext.Provider value={{ socket, online }}>
